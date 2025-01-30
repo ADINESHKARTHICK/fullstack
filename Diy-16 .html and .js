1b82bfddbbeb1c97c997a1b375e72309048a5c95
ADINESHKARTHICK <dinesha1406@gmail.com>
2025-01-30 20:49:57 +0530

    Diy-16 .html and .js

---
+++ Diy-16 .html and .js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        var num=25;
+        var str='Hello';
+        var bigint=590295810358705651712n;
+        var bool=true;
+        var und=undefined;
+        var obj=null;
+        var sym=Symbol();
+        console.log('number string bigint bool und obj sym');
+        console.log(num,str,bigint,bool,und,obj,sym);
+        var arr=[1,2,3,4,5];
+        var object={
+            name:'Bablu',
+            age:20,
+            email: 'bablu@gmail.com',
+        };
+        var date= new Date();
+        console.log('object object object function');
+        console.log(arr,object,date);
+        console.log(date);
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
